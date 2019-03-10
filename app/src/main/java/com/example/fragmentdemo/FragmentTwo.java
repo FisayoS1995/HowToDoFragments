71d94d242a8261d86097f232982ffa0fbbd5a844
Fisayo Samuel <fisayo_s@yahoo.co.uk>
2019-03-10 22:49:31 +0000

    how to implement fragments

---
+++ app/src/main/java/com/example/fragmentdemo/FragmentTwo.java
@@ -0,0 +1,35 @@
+package com.example.fragmentdemo;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class FragmentTwo extends Fragment {
+
+    private TextView tvStatus;
+    private Button btnShowStatus;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_two, container, false);
+        tvStatus =  view.findViewById(R.id.tvstatus);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        btnShowStatus = view.findViewById(R.id.btnShow);
+        btnShowStatus.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                tvStatus.setText("Clicked from Fragment Two");
+            }
+        });
+    }
+}
